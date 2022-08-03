007b1def1cc9a14678494c2fbb7544e797c57800
Yicong Fu <fuyicong1996@gmail.com>
2022-08-03 11:17:05 -0400

    a toy_elasticity.cpp for performance test

--- examples/cpp/toy_elasticity.cpp
+++ examples/cpp/toy_elasticity.cpp
@@ -12,19 +12,20 @@
 
 using namespace A2D;
 
-int main(int argc, char* argv[]) {
-  static const index_t SPATIAL_DIM = 2;
+void main_body(int argc, char* argv[]) {
+  static const index_t SPATIAL_DIM = 3;
   typedef index_t I;
-  typedef std::complex<double> T;
   // typedef std::complex<double> T;
-  typedef BasisOps<SPATIAL_DIM, QuadBiLinearBasisFunc, Quad4ptQuadrature> Basis;
+  typedef double T;
+  typedef BasisOps<SPATIAL_DIM, HexTriLinearBasisFunc, Hex8ptQuadrature> Basis;
   typedef ElasticityPDEInfo<SPATIAL_DIM, I, T> PDE;
 
-  const index_t nx = 32;
-  const index_t ny = 32;
-  const index_t nnodes = (nx + 1) * (ny + 1);
-  const index_t nelems = nx * ny;
-  const index_t nbcs = (ny + 1);
+  const index_t nx = 64;
+  const index_t ny = 64;
+  const index_t nz = 64;
+  const index_t nnodes = (nx + 1) * (ny + 1) * (nz + 1);
+  const index_t nelems = nx * ny * nz;
+  const index_t nbcs = (ny + 1) * (nz + 1);
 
   auto model = std::make_shared<FEModel<I, T, PDE>>(nnodes, nbcs);
   auto element = std::make_shared<NonlinElasticityElement<I, T, Basis>>(nelems);
@@ -33,47 +34,62 @@ int main(int argc, char* argv[]) {
   // Set the boundary conditions
   auto bcs = model->get_bcs();
   index_t index = 0;
-  for (int j = 0; j < ny + 1; j++) {
-    int i = 0;
-    int node = i + (nx + 1) * j;
+  for (int k = 0; k < nz + 1; k++) {
+    for (int j = 0; j < ny + 1; j++) {
+      int i = 0;
+      int node = i + (nx + 1) * (j + (ny + 1) * k);
 
-    // Set the boundary conditions
-    bcs(index, 0) = node;
-    for (int ii = 0; ii < SPATIAL_DIM; ii++) {
-      bcs(index, 1) |= 1U << ii;
+      // Set the boundary conditions
+      bcs(index, 0) = node;
+      for (int ii = 0; ii < 3; ii++) {
+        bcs(index, 1) |= 1U << ii;
+      }
+      index++;
     }
-    index++;
   }
 
   // Set the connectivity
   auto conn = element->get_conn();
-  for (int j = 0; j < ny; j++) {
-    for (int i = 0; i < nx; i++) {
-      int elem = i + nx * j;
+  for (int k = 0; k < nz; k++) {
+    for (int j = 0; j < ny; j++) {
+      for (int i = 0; i < nx; i++) {
+        int elem = i + nx * (j + ny * k);
 
-      int conn_coord[4];
-      for (int jj = 0, index = 0; jj < 2; jj++) {
-        for (int ii = 0; ii < 2; ii++, index++) {
-          conn_coord[index] = (i + ii) + (nx + 1) * (j + jj);
+        int conn_coord[8];
+        for (int kk = 0, index = 0; kk < 2; kk++) {
+          for (int jj = 0; jj < 2; jj++) {
+            for (int ii = 0; ii < 2; ii++, index++) {
+              conn_coord[index] =
+                  (i + ii) + (nx + 1) * ((j + jj) + (ny + 1) * (k + kk));
+            }
+          }
         }
-      }
 
-      // Convert to the correct connectivity
-      conn(elem, 0) = conn_coord[0];
-      conn(elem, 1) = conn_coord[1];
-      conn(elem, 2) = conn_coord[3];
-      conn(elem, 3) = conn_coord[2];
+        // Convert to the correct connectivity
+        conn(elem, 0) = conn_coord[0];
+        conn(elem, 1) = conn_coord[1];
+        conn(elem, 2) = conn_coord[3];
+        conn(elem, 3) = conn_coord[2];
+
+        conn(elem, 4) = conn_coord[4];
+        conn(elem, 5) = conn_coord[5];
+        conn(elem, 6) = conn_coord[7];
+        conn(elem, 7) = conn_coord[6];
+      }
     }
   }
 
   // Set the node locations
   auto X = model->get_nodes();
-  for (int j = 0; j < ny + 1; j++) {
-    for (int i = 0; i < nx + 1; i++) {
-      int node = i + (nx + 1) * j;
+  for (int k = 0; k < nz + 1; k++) {
+    for (int j = 0; j < ny + 1; j++) {
+      for (int i = 0; i < nx + 1; i++) {
+        int node = i + (nx + 1) * (j + (ny + 1) * k);
 
-      X(node, 0) = 1.0 * i / nx;
-      X(node, 1) = 1.0 * j / ny;
+        X(node, 0) = 1.0 * i / nx;
+        X(node, 1) = 1.0 * j / ny;
+        X(node, 2) = 1.0 * k / nz;
+      }
     }
   }
 
@@ -139,72 +155,15 @@ int main(int argc, char* argv[]) {
   amg->cg(*residual, *solution, monitor, max_iters);
   delete t;
 
-  // Set the solution
-  model->set_solution(solution);
-
-  agg_functional->compute_offset();
-  functional->eval_functional();
-
-  // Compute the adjoint right-hand-side
-  auto dfdu = model->new_solution();
-  functional->eval_dfdu(dfdu);
-  dfdu->scale(-1.0);
-  model->zero_bcs(dfdu);
-
-  // Compute the adjoint variables
-  auto adjoint = model->new_solution();
-  t = new Timer("Adjoint solution");
-  amg->mg(*dfdu, *adjoint, monitor, max_iters);
-  delete t;
-
-  // Complete the adjoint derivative
-  auto dfdx =
-      std::shared_ptr<A2D::MultiArray<T, A2D::CLayout<1>>>(x->duplicate());
-  dfdx->zero();
-  functional->eval_dfdx(dfdx);
-  model->add_adjoint_dfdx(adjoint, dfdx);
-
-  // Compute a projected derivative and test against complex step
-  auto px =
-      std::shared_ptr<A2D::MultiArray<T, A2D::CLayout<1>>>(x->duplicate());
-  px->random();
-  T ans = dfdx->dot(*px);
-
-  // Set the new desigh variable values
-  double dh = 1e-30;
-  x->fill(1.0);
-  x->axpy(T(0.0, dh), *px);
-  model->set_design_vars(x);
-  model->jacobian(J);
-  amg->update();
-
-  t = new Timer("CG solution");
-  solution->zero();
-  amg->cg(*residual, *solution, monitor, max_iters);
-  delete t;
-
-  // Set the solution
-  model->set_solution(solution);
-
-  // Compute the complex-step result
-  auto fun_val = functional->eval_functional();
-  std::cout << fun_val << std::endl;
-  T fd = fun_val.imag() / dh;
-
-  std::cout << "Complex-step result: " << std::setw(20) << std::setprecision(16)
-            << fd.real() << std::endl;
-  std::cout << "Adjoint-data result: " << std::setw(20) << std::setprecision(16)
-            << ans.real() << std::endl;
-  std::cout << "Relative error:      " << std::setw(20) << std::setprecision(16)
-            << (ans.real() - fd.real()) / ans.real() << std::endl;
-
   ToVTK<decltype(element->get_conn()), decltype(model->get_nodes())> vtk(conn,
                                                                          X);
   vtk.write_mesh();
   vtk.write_sol("ux", *solution, 0);
   vtk.write_sol("uy", *solution, 1);
-  vtk.write_sol("adjoint_x", *adjoint, 0);
-  vtk.write_sol("adjoint_y", *adjoint, 1);
+  vtk.write_sol("uz", *solution, 2);
+}
 
-  return (0);
+int main(int argc, char* argv[]) {
+  Timer t("main");
+  main_body(argc, argv);
 }
